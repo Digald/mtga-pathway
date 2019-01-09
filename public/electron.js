0eaca1515a19502840f7ff39aa1bf109cc5a14ad
unknown <nosracer_gt@sbcglobal.net>
2019-01-09 01:07:58 -0600

    added windows file path to log file and was able to read it with fs.readFile

--- public/electron.js
+++ public/electron.js
@@ -1,6 +1,7 @@
 // Modules to control application life and create native browser window
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
+const fs = require("fs");
 const isDev = require("electron-is-dev");
 
 // Keep a global reference of the window object, if you don't, the window will
@@ -54,3 +55,9 @@ app.on("activate", function() {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+// console.log(process.env.USERNAME);
+const user = process.env.USERNAME;
+const winAbsPath = `C:/Users/${user}/AppData/LocalLow/Wizards Of The Coast/MTGA/output_log.txt`;
+const logData = fs.readFileSync(winAbsPath).toString();
+console.log(logData);
