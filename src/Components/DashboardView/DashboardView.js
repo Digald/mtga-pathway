7308c2db0afc37fa0158de902894a346becfcdd8
unknown <nosracer_gt@sbcglobal.net>
2019-02-07 21:57:42 -0600

    Added mana svg folder and continued making components for DashboardView.js

--- src/Components/DashboardView/DashboardView.js
+++ src/Components/DashboardView/DashboardView.js
@@ -2,15 +2,22 @@ import React, { Component } from "react";
 import "./DashboardView.css";
 // Components
 import DeckGrid from "../DeckGrid/DeckGrid";
+import DisplayCards from "../DisplayCards/DisplayCards";
 
 class DashboardView extends Component {
   render() {
     return (
       <div className="DashboardView">
-        <h2 className="DashboardView__title">Saved Decks</h2>
-        <DeckGrid />
-        <h2 className="DashboardView__title">Newly Added To Collection</h2>
-        <div />
+        <div className="DashboardView__container">
+          <h2 className="DashboardView__container__title">Saved Decks</h2>
+          <DeckGrid />
+        </div>
+        <div className="DashboardView__container">
+          <h2 className="DashboardView__container__title">
+            Newly Added To Collection
+          </h2>
+          <DisplayCards />
+        </div>
       </div>
     );
   }
---
+++ src/Components/DisplayCards/DisplayCards.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./DisplayCards.css";
+
+class DisplayCards extends Component {
+  render() {
+    return (
+      <div className="DisplayCards">
+      </div>
+    );
+  }
+}
+
+export default DisplayCards;
