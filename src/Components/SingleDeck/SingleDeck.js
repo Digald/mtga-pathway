fc5c0728b5708d9a68e5fe9eb0c929a2d47ec4ab
unknown <nosracer_gt@sbcglobal.net>
2019-02-17 23:31:43 -0600

    Working on gettings styles for the deck previews

--- src/Components/SingleDeck/SingleDeck.js
+++ src/Components/SingleDeck/SingleDeck.js
@@ -3,20 +3,23 @@ import "./SingleDeck.css";
 
 class SingleDeck extends Component {
   render() {
-    const { name, colors } = this.props.deck;
+    const { name, colors, url } = this.props.deck;
     const image = this.props.deck.deckList[0].image;
     return (
       <div className="SingleDeck">
         <div className="SingleDeck__header">
           <p>{name}</p>
           <div className="SingleDeck__header__colors">
-            {colors.map(color => {
-              return <p>{color}</p>;
+            {colors.map((color, i) => {
+              return <p key={i}>{color}</p>;
             })}
           </div>
         </div>
         <img className="SingleDeck__background" src={image} alt="Preview" />
-        <div className="SingleDeck__percentages" />
+        <div className="SingleDeck__percentages">
+          <p>% Complete w/wildcards</p>
+          <p>% Complete Wo/wildcards</p>
+        </div>
       </div>
     );
   }
