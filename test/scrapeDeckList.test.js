6c5528a35c020487ba46af36a644ff05f1c66182
unknown <nosracer_gt@sbcglobal.net>
2019-06-19 22:19:34 -0500

    Still trying to get passing tests for scrapeDeckList.js

--- test/scrapeDeckList.test.js
+++ test/scrapeDeckList.test.js
@@ -1,5 +1,32 @@
 const scrapeDeckList = require("../main/functions/scrapeDeckList");
 jest.setTimeout(10000);
+
+expect.extend({
+  toContainObject(received, argument) {
+    const pass = this.equals(
+      received,
+      expect.arrayContaining([expect.objectContaining(argument)])
+    );
+
+    if (pass) {
+      return {
+        message: () =>
+          `expected ${this.utils.printReceived(
+            received
+          )} not to contain object ${this.utils.printExpected(argument)}`,
+        pass: true
+      };
+    } else {
+      return {
+        message: () =>
+          `expected ${this.utils.printReceived(
+            received
+          )} to contain object ${this.utils.printExpected(argument)}`,
+        pass: false
+      };
+    }
+  }
+});
 describe("Confirm that deck list is properly added from deck metadata", () => {
   const singleDeck = {
     name: "Bant Explore",
@@ -7,7 +34,23 @@ describe("Confirm that deck list is properly added from deck metadata", () => {
     url: "https://www.mtggoldfish.com/archetype/standard-bant-explore#arena"
   };
 
-  it("Should return an array", async () => {
-    expect(await scrapeDeckList(singleDeck)).toEqual(expect.any(Array));
+  // it("Should return an array", async () => {
+  //   expect(await scrapeDeckList(singleDeck)).toEqual(expect.any(Array));
+  // });
+
+  // it("Should contain an object in each index", async () => {
+  //   expect(await scrapeDeckList(singleDeck)).toEqual(expect.any(Object));
+  // });
+
+  it("Should contain certain properties in each object", async () => {
+    expect(await scrapeDeckList(singleDeck)).toContainObject({
+      name: expect.any(String);
+      // name: expect.any(String),
+      // quantity: expect.any(String),
+      // image: expect.any(String),
+      // mana_cost: expect.any(Array),
+      // rarity: expect.any(String),
+      // type: expect.any(String)
+    });
   });
 });
