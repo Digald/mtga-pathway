41758d545e26bd1f376cd3a2c4959db7eb320612
unknown <nosracer_gt@sbcglobal.net>
2019-06-23 20:47:34 -0500

    Attempted to get one more working test on scrapeDeckList.test.js

--- test/scrapeDeckList.test.js
+++ test/scrapeDeckList.test.js
@@ -17,7 +17,20 @@ describe("Confirm that deck list is properly added from deck metadata", async ()
     url: "https://www.mtggoldfish.com/archetype/standard-bant-explore#arena"
   };
 
+  // const returnObj = {
+  //   name: expect.any(String),
+  //   quantity: expect.any(String),
+  //   image: expect.any(String),
+  //   mana_cost: expect.any(Array),
+  //   rarity: expect.any(String),
+  //   type: expect.any(String)
+  // };
+
   it("Should return an array", async () => {
     expect(await scrapeDeckList(singleDeck)).toEqual(expect.any(Array));
   });
+
+  // it("Should return certain properties in each object in array", async () => {
+  //   expect(await scrapeDeckList(singleDeck)).toContain(returnObj);
+  // });
 });
